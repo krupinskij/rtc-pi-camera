dcbb7c224e758a20c662e7482604f01c9fed4e30
Jan Krupiński <jk.krupinski@gmail.com>
2021-12-17 23:58:38 +0100

    WebRTC: Establish connection

--- index.js
+++ index.js
@@ -3,10 +3,31 @@ const io = require('socket.io-client');
 
 const SERVER_URI = 'http://localhost:3030/';
 
+const map = new Map();
+
 const socket = io(SERVER_URI);
 
 socket.on('connect', () => {
   console.log('Camera connected');
 
   socket.emit('save-code', config.code);
+});
+
+socket.on('get-offer-from-camera', userId => {
+  const connection = new RTCPeerConnection();
+  map.set(userId, connection);
+  let localDescription;
+  connection.addEventListener('icecandidate', event => {
+    if (!localDescription) {
+      localDescription = connection.localDescription;
+
+      socket.emit('send-offer-to-server', userId, localDescription);
+    }
+  });
+  connection.createOffer().then(o => localConnection.setLocalDescription(o));
+});
+
+socket.on('send-answer-to-camera', (userId, answer) => {
+  const connection = map.get(userId);
+  connection.setRemoteDescription(answer).then(() => console.log('WebRTC: Connection established'));
 });
