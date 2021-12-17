924a6d96b767b6b961b1daf0577aaae05df4542f
Jan Krupiński <jk.krupinski@gmail.com>
2021-12-17 23:29:19 +0100

    WebRTC: Install socket io

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-console.log('Hello world');
+const config = require('./config.json');
+const io = require('socket.io-client');
+
+const SERVER_URI = 'http://localhost:3030/';
+
+const socket = io(SERVER_URI);
+
+socket.on('connect', () => {
+  console.log('Camera connected');
+
+  socket.emit('save-code', config.code);
+});
